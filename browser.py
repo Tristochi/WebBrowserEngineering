73ec7a0e3fce22fd6155a9d72fb2373087b0dd86
Tristochi <tristanlszakacs@gmail.com>
2025-07-27 14:06:18 -0400

    Adds scrollbar instead of manually handling scroll function.

--- browser.py
+++ browser.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from url import *
 import main  
 
@@ -10,24 +11,41 @@ class Browser:
         self._VSTEP = 18
         self._SCROLL_STEP = 100
         self.window = Tk()
-        self.canvas = Canvas(
-            self.window,
-            width=self._WIDTH,
-            height=self._HEIGHT
-        )
-        self.display_list = ""
+        self.display_list = [[self._WIDTH,self._HEIGHT,""]]
         self.scroll = 0
         self.window.bind("<Down>", self.scrolldown)
         self.window.bind("<Up>", self.scrollup)
         self.window.bind("<MouseWheel>", self.scrollwheel)
         self.window.bind("<Configure>", self.resize)
-        self.canvas.pack(fill=BOTH, expand=1)
+        
+        self.canvas = Canvas(
+            self.window,
+            width=self._WIDTH,
+            height=self._HEIGHT,
+            scrollregion=(0, 0, self.display_list[-1][1], self._WIDTH)
+        )
+        self.style = ttk.Style(self.window)
+        self.style.theme_use("default")
+        self.style.configure("Blue.Vertical.TScrollbar",
+                             background="blue",
+                             troughcolor="lightblue",
+                             arrowcolor="white")
+        self.scrollbar = ttk.Scrollbar(self.window, orient=VERTICAL, command=self.canvas.yview,style="Blue.Vertical.TScrollbar")
+
+        self.canvas.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.pack(side=RIGHT,fill=Y)
+        self.canvas.pack(side=LEFT,fill=BOTH, expand=1)
+        
         self.text = ""
     
+    def canvas_config(self, e):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
     def resize(self, e):
         self._HEIGHT = e.height
         self._WIDTH = e.width
         self.display_list = main.layout(self.text, width=e.width, height=e.height)
+
         self.draw()
 
     def load(self, url):
@@ -53,23 +71,31 @@ class Browser:
 
     def draw(self):
         self.canvas.delete("all")
+
         for x, y, c in self.display_list:
             # Avoid drawing characters that are not on screen to speed up the draw time.
-            if y > self.scroll + self._HEIGHT: continue 
-            if y + self._VSTEP < self.scroll: continue
-            self.canvas.create_text(x, y - self.scroll, text=c)
+            #if y > self.scroll + self._HEIGHT: continue 
+            #if y + self._VSTEP < self.scroll: continue
+            #self.canvas.create_text(x, y - self.scroll, text=c)
+            self.canvas.create_text(x,y,text=c)
+        
+        width, self._HEIGHT, c = self.display_list[-1]
+        self.canvas.config(scrollregion=self.canvas.bbox("all"))
     
     def scrolldown(self,e):
         last_x_pos, last_y_pos, c = self.display_list[-2]
-        print(f"Current scroll: {self.scroll}, Scroll pos + scroll step: {self.scroll+self._SCROLL_STEP}, Last Y pos: {last_y_pos}")
-        if last_y_pos - self.scroll > self._HEIGHT:
-            self.scroll += self._SCROLL_STEP
-            self.draw()
+        #print(f"Current scroll: {self.scroll}, Scroll pos + scroll step: {self.scroll+self._SCROLL_STEP}, Last Y pos: {last_y_pos}")
+        #if last_y_pos - self.scroll > self._HEIGHT:
+        #self.scroll += self._SCROLL_STEP
+        self.canvas.yview_scroll(int(-1*(e.delta/120)), "units")
+            #self.draw()
     
     def scrollup(self, e):
-        if self.scroll > 0:
-            self.scroll -= self._SCROLL_STEP 
-            self.draw()
+        self.canvas.yview_scroll(int(-1*(e.delta/120)), "units")
+        #if self.scroll > 0:
+            #self.scroll -= self._SCROLL_STEP 
+            
+            #self.draw()
     
     def scrollwheel(self, e):
         if e.delta > 0:
